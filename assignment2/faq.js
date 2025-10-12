43456e630d00aa8b363162c1915250b0e7147ba5
Moldirkab <155832918+Moldirkab@users.noreply.github.com>
2025-10-12 17:18:45 +0500

    Create faq.js

---
+++ assignment2/faq.js
@@ -0,0 +1,19 @@
+function setupFAQToggles() {
+  const questions = document.querySelectorAll(".faq-question");
+
+  questions.forEach(question => {
+    question.addEventListener("click", () => {
+      question.classList.toggle("active");
+
+      const answer = question.nextElementSibling;
+
+      if (answer.style.display === "block") {
+        answer.style.display = "none";
+      } else {
+        answer.style.display = "block";
+      }
+    });
+  });
+}
+
+document.addEventListener("DOMContentLoaded", setupFAQToggles);
